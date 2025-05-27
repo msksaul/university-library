39301adb8d481dc51e09744edfadca97450d89d3
Melvin Calel <msksaul@gmail.com>
2025-05-27 17:07:30 -0600

    Send Emails and Workflows

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -2,7 +2,7 @@ import { serve } from "@upstash/workflow/nextjs";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
-//import { sendEmail } from "@/lib/workflow";
+import { sendEmail } from "@/lib/workflow";
 
 type UserState = "non-active" | "active";
 
